3929f015753660345ac3868265f1db6f5d8906fd
Afaq Khan <afaqk3203@gmail.com>
2023-08-15 22:29:34 +0500

    design login page

--- index.php
+++ index.php
@@ -1,68 +1,111 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Y School System</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link rel="stylesheet" href="css/style.css">
     <link rel="icon" href="logo.png">
 </head>
+
 <body class="body-home">
 
-<div class="black-fill">
+    <div class="black-fill">
 
-<br><br>
+        <br><br>
 
 
-  <div class="container">
-    <nav class="navbar navbar-expand-lg navbar-light bg-light " id="homeNav">
-    <div class="container-fluid">
-    <a class="navbar-brand" href="#">
-      <img src="logo.png" width="40">
-    </a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Home</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">About</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Contact</a>
-        </li>
-       
-        
-      </ul>
-      <ul class="navbar-nav me-right mb-2 mb-lg-0">
-      <li class="nav-item">
-          <a class="nav-link" href="#">Login</a>
+        <div class="container">
+            <nav class="navbar navbar-expand-lg navbar-light bg-light " id="homeNav">
+                <div class="container-fluid">
+                    <a class="navbar-brand" href="#">
+                        <img src="logo.png" width="40">
+                    </a>
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                        data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                        aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                            <li class="nav-item">
+                                <a class="nav-link active" aria-current="page" href="#">Home</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#about">About</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#contact">Contact</a>
+                            </li>
 
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Sign Up</a>
-        </li>
-     
-      </ul>
-     
+
+                        </ul>
+                        <ul class="navbar-nav me-right mb-2 mb-lg-0">
+                            <li class="nav-item">
+                                <a class="nav-link" href="login.php">Login</a>
+
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Sign Up</a>
+                            </li>
+
+                        </ul>
+
+                    </div>
+                </div>
+            </nav>
+
+            <section class="welcome-text d-flex justify-content-center align-items-center flex-column">
+                <img src="logo.png" alt="icon">
+                <h4>Welcome to Y School</h4>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+            </section>
+            <section id="about" class=" d-flex justify-content-center align-items-center flex-column">
+                <div class="card mb-3 card-1">
+                    <div class="row g-0">
+                        <div class="col-md-4">
+                            <img src="logo.png" class="img-fluid rounded-start" alt="logo">
+                        </div>
+                        <div class="col-md-8">
+                            <div class="card-body">
+                                <h5 class="card-title">About Us</h5>
+                                <p class="card-text">This is a wider card with supporting text below as a natural
+                                    lead-in to additional content. This content is a little bit longer.</p>
+                                <p class="card-text"><small class="text-muted">Y School</small></p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+            <section id="contact" class=" d-flex justify-content-center align-items-center flex-column">
+                <form>
+                    <h3>Contact Us</h3>
+                    <div class="mb-3">
+                        <label for="exampleInputEmail1" class="form-label">Email address</label>
+                        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                        <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+                    </div>
+                    <div class="mb-3">
+                        <label for="exampleInputPassword1" class="form-label">Name</label>
+                        <input type="text" class="form-control">
+                    </div>
+                    <textarea class="form-control" name="details" placeholder="Mention Details" rows="4"></textarea>
+                    <button type="submit" class="btn btn-primary mt-2">Send</button>
+                </form>
+            </section>
+            <div class="text-center text-light">Copyrights &copy; Y School. All rights reserved</div>
+        </div>
+
     </div>
-     </div>
-     </nav>
 
-    <section class="welcome-text d-flex justify-content-center align-items-center flex-column">
-<img src="logo.png" alt="icon" >
-<h4>Welcome to Y School</h4>
-<p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
-    </section>
-  </div>
-
-</div>
-
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
+        </script>
 
 </body>
+
 </html>
---
+++ login.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login Y School </title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="icon" href="logo.png">
+</head>
+
+<body class="body-home">
+
+    <div class="black-fill">
+
+        <br><br>
+
+
+        <div class="d-flex justify-content-center align-items-center flex-column">
+            <form class="login">
+                <div class="text-center">
+                    <img src="logo.png" width="100">
+                </div>
+                <h3>Login</h3>
+                <div class="mb-3">
+                    <label class="form-label">Name</label>
+                    <input type="text" class="form-control">
+                </div>
+                <div class="mb-3">
+                    <label for="exampleInputPassword1" class="form-label">Password</label>
+                    <input type="password" class="form-control" id="exampleInputPassword1">
+                </div>
+
+                <div class="mb-3">
+                    <label class="form-label">Login As</label>
+                    <select class="form-control">
+                        <option value="1">Admin</option>
+                        <option value="2">Student</option>
+                        <option value="3">Teacher</option>
+
+
+                    </select>
+                </div>
+
+                <button type="submit" class="btn btn-primary">Login</button>
+                <a href="index.php" class="text-decoration-none fw-bolder ">Back to Home</a>
+            </form>
+
+
+            <br>
+            <br>
+
+            <div class="text-center text-light">Copyrights &copy; Y School. All rights reserved</div>
+
+
+        </div>
+
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
+        </script>
+
+</body>
+
+</html>
