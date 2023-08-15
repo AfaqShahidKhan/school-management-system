7aebb8cdd99e3d2ffb65fd12a3a8e9b5a7ac6153
Afaq Khan <afaqk3203@gmail.com>
2023-08-15 15:07:10 +0500

    initialize project

---
+++ index.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Y School System</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="icon" href="logo.png">
+</head>
+<body class="body-home">
+
+<div class="black-fill">
+
+<br><br>
+
+
+  <div class="container">
+    <nav class="navbar navbar-expand-lg navbar-light bg-light " id="homeNav">
+    <div class="container-fluid">
+    <a class="navbar-brand" href="#">
+      <img src="logo.png" width="40">
+    </a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">About</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Contact</a>
+        </li>
+       
+        
+      </ul>
+      <ul class="navbar-nav me-right mb-2 mb-lg-0">
+      <li class="nav-item">
+          <a class="nav-link" href="#">Login</a>
+
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Sign Up</a>
+        </li>
+     
+      </ul>
+     
+    </div>
+     </div>
+     </nav>
+
+    <section class="welcome-text d-flex justify-content-center align-items-center flex-column">
+<img src="logo.png" alt="icon" >
+<h4>Welcome to Y School</h4>
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+    </section>
+  </div>
+
+</div>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+
+</body>
+</html>
