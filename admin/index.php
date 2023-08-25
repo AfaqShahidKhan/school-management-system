bd731db1d8b8089d4e9140ec9eadb7eb1fc7bb39
Afaq Khan <afaqk3203@gmail.com>
2023-08-25 22:31:25 +0500

    icon pages

--- admin/index.php
+++ admin/index.php
@@ -37,6 +37,27 @@ if (isset($_SESSION['admin_id'])&& isset($_SESSION['role'])&& isset($_SESSION['u
             <div class="row row-cols-5">
                 <a href="" class="col btn btn-dark m-3 py-3"><i class="fa fa-user-plus fs-1"
                         aria-hidden="true"></i><br>Teachers</a>
+                <a href="" class="col btn btn-dark m-3 py-3"><i class="fa fa-graduation-cap fs-1"
+                        aria-hidden="true"></i> </i><br>
+                    Students</a>
+                <a href="" class="col btn btn-dark m-3 py-3"><i class="fa fa-pencil fs-1" aria-hidden="true"></i><br>
+                    Registrar Office</a>
+                <a href="" class="col btn btn-dark m-3 py-3"><i class="fa fa-cubes fs-1" aria-hidden="true"></i><br>
+                    Class</a><a href="" class="col btn btn-dark m-3 py-3"><i class="fa fa-columns fs-1"
+                        aria-hidden="true"></i><br>
+                    Section</a>
+                <a href="" class="col btn btn-dark m-3 py-3"><i class="fa fa-calendar fs-1" aria-hidden="true"></i><br>
+                    Schedule</a>
+                <a href="" class="col btn btn-dark m-3 py-3"><i class="fa fa-book fs-1" aria-hidden="true"></i><br>
+                    Course</a>
+                <a href="" class="col btn btn-dark m-3 py-3"><i class="fa fa-envelope fs-1" aria-hidden="true"></i><br>
+                    Message</a>
+                <a href="" class="col btn btn-primary m-4 py-3 col-5"><i class="fa fa-cogs fs-1"
+                        aria-hidden="true"></i><br>
+                    Settings</a>
+                <a href="" class="col btn btn-warning m-4 py-3 col-5"><i class="fa fa-sign-out fs-1"
+                        aria-hidden="true"></i><br>
+                    Logout</a>
             </div>
         </div>
     </div>
